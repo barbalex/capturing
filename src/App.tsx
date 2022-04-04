30d5dc8c9268dd770b0334948243bd49d5b460e3
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-04 20:47:40 +0200

    initiate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
 
 import MobxStore from './store'
+import initiateApp from './utils/initiateApp'
+import materialTheme from './utils/materialTheme'
+import { Provider as MobxProvider } from './storeContext'
 
 function App() {
   console.log('App rendering')
   const store = MobxStore.create()
+  // TODO: initiate app
 
-  return <div>hi</div>
+  return (
+    <ThemeProvider theme={materialTheme}>
+      <MobxProvider value={store}>
+        <div>hi</div>
+      </MobxProvider>
+    </ThemeProvider>
+  )
 }
 
 export default App
