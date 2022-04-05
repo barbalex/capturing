c2f4beeed54f08301a6b50d187471ee6a8f2a1b3
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-05 14:12:25 +0200

    optimize

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-// import Layout from '../components/Layout'
-import ErrorBoundary from '../components/shared/ErrorBoundary'
 
 const FourOhFour = () => {
-  return <ErrorBoundary>sorry, nothing here</ErrorBoundary>
+  return <p>sorry, nothing here</p>
 }
 
 export default FourOhFour
