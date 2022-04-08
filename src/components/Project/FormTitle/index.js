595788ba61df3b3f15b6a6f6d59a9284ed3993d3
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-08 22:43:30 +0200

    work on project form

---
+++ src/components/Project/FormTitle/index.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import StoreContext from '../../../storeContext'
+import FilterTitle from '../../shared/FilterTitle'
+import FormTitle from './FormTitle/FormTitle'
+
+const ProjectFormTitleChooser = ({ row }) => {
+  const store = useContext(StoreContext)
+  const showFilter = false // TODO:
+  const showHistory = false // TODO:
+  const setShowHistory = () => {} // TODO:
+
+  const [countState, setCountState] = useState({
+    totalCount: 0,
+    filteredCount: 0,
+  })
+  useEffect(() => {
+    // setCountState({ totalCount, filteredCount })
+  }, [])
+
+  const { totalCount, filteredCount } = countState
+
+  if (showFilter) {
+    return (
+      <FilterTitle
+        title="Art"
+        table="art"
+        totalCount={totalCount}
+        filteredCount={filteredCount}
+      />
+    )
+  }
+
+  return (
+    <FormTitle
+      row={row}
+      totalCount={totalCount}
+      filteredCount={filteredCount}
+      showHistory={showHistory}
+      setShowHistory={setShowHistory}
+    />
+  )
+}
+
+export default observer(ProjectFormTitleChooser)
