de9fd11b416e2e15aa71ea408b21bc46fe725704
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-10 18:10:07 +0200

    set theme

--- src/utils/materialTheme.js
+++ src/utils/materialTheme.js
@@ -1,11 +1,14 @@
 import { createTheme } from '@mui/material/styles'
 import { orange, purple } from '@mui/material/colors'
 
+// see: https://mui.com/material-ui/customization/theming/
+
 const theme = createTheme({
   palette: {
     primary: { main: purple[900] },
     secondary: { main: orange[900] },
   },
+  typography: { fontFamily: ['Roboto'] },
 })
 
 export default theme
