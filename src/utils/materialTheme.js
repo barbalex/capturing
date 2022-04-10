7ae1a98d99ce95ca6037924a174ecefa85a8e483
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-10 18:27:29 +0200

    add start of theming

--- src/utils/materialTheme.js
+++ src/utils/materialTheme.js
@@ -1,7 +1,40 @@
 import { createTheme } from '@mui/material/styles'
-import { orange, purple } from '@mui/material/colors'
+import { orange, purple, amber, deepOrange, grey } from '@mui/material/colors'
 
 // see: https://mui.com/material-ui/customization/theming/
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...purple,
+      ...(mode === 'dark' && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === 'dark' && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === 'light'
+        ? {
+            primary: grey[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: '#fff',
+            secondary: grey[500],
+          }),
+    },
+  },
+  typography: { fontFamily: ['Roboto'] },
+})
+
+export const darkModeTheme = createTheme(getDesignTokens('dark'))
+export const lightModeTheme = createTheme(getDesignTokens('light'))
 
 const theme = createTheme({
   palette: {
