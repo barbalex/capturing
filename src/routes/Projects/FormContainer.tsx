19f0ffa5394c33b4309be24d0e3c7eaa372c325c
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-12 00:21:35 +0200

    style

--- src/routes/Projects/FormContainer.tsx
+++ src/routes/Projects/FormContainer.tsx
@@ -1,14 +1,19 @@
 import styled from 'styled-components'
 import SimpleBar from 'simplebar-react'
 
+const StyledSimplebar = styled(SimpleBar)`
+  .simplebar-content {
+    height: 100%;
+  }
+`
 const Container = styled.div`
   height: 100%;
 `
 
 const FormContainer = ({ children }) => (
-  <SimpleBar style={{ maxHeight: '100%', height: '100%' }}>
+  <StyledSimplebar style={{ maxHeight: '100%', height: '100%' }}>
     <Container>{children}</Container>
-  </SimpleBar>
+  </StyledSimplebar>
 )
 
 export default FormContainer
