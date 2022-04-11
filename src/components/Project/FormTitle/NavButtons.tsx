314ca5d307b7ef4f2fdbf547db71a9aead959064
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-11 23:39:22 +0200

    remove not needed

--- src/components/Project/FormTitle/NavButtons.tsx
+++ src/components/Project/FormTitle/NavButtons.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
-import { FaLongArrowAltUp, FaLongArrowAltDown } from 'react-icons/fa'
+import { FaLongArrowAltUp } from 'react-icons/fa'
 
 import StoreContext from '../../../storeContext'
 
@@ -13,33 +13,11 @@ const ProjectNavButtons = () => {
     removeOpenNode(activeNodeArray)
     setActiveNodeArray(activeNodeArray.slice(0, -1))
   }, [activeNodeArray, removeOpenNode, setActiveNodeArray])
-  const onClickToSammlungen = useCallback(
-    () => setActiveNodeArray([...activeNodeArray, 'Sammlungen']),
-    [activeNodeArray, setActiveNodeArray],
-  )
-  const onClickToKulturen = useCallback(
-    () => setActiveNodeArray([...activeNodeArray, 'Kulturen']),
-    [activeNodeArray, setActiveNodeArray],
-  )
 
   return (
     <>
       <IconButton title="Zur Liste" onClick={onClickUp} size="large">
         <FaLongArrowAltUp />
-      </IconButton>
-      <IconButton
-        title="Zu den Sammlungen dieser Projektes"
-        onClick={onClickToSammlungen}
-        size="large"
-      >
-        <FaLongArrowAltDown />
-      </IconButton>
-      <IconButton
-        title="Zu den Kulturen dieser Projektes"
-        onClick={onClickToKulturen}
-        size="large"
-      >
-        <FaLongArrowAltDown />
       </IconButton>
     </>
   )
