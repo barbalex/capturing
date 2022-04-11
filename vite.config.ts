42ee74b97d9bc4695637d2afcfd19e3fddd3a6d2
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-11 13:24:09 +0200

    generate service worker

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({}), react()],
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'Capturing',
+        short_name: 'Capturing data',
+        description: 'Capture data: whatever, wherever, whenever, whoever',
+        theme_color: '#4a148c',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 })
