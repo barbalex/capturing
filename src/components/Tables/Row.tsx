a7ccdb7a50a800c3365744aa267d7e76a4b8edf2
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-12 13:33:45 +0200

    optimize

--- src/components/Tables/Row.tsx
+++ src/components/Tables/Row.tsx
@@ -1,17 +1,16 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useLiveQuery } from 'dexie-react-hooks'
+import ListItem from '@mui/material/ListItem'
+import { Link } from 'react-router-dom'
 
 import StoreContext from '../../storeContext'
 import constants from '../../utils/constants'
 import { dexie, Project } from '../../dexieClient'
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+const StyledListItem = styled(ListItem)`
   min-height: ${constants.singleRowHeight};
   border-top: thin solid rgba(74, 20, 140, 0.1);
   border-bottom: ${(props) => (props['data-last'] ? '1px' : 'thin')} solid
@@ -19,12 +18,6 @@ const Row = styled.div`
   border-collapse: collapse;
   margin: -1px 0;
   padding: 10px;
-  cursor: pointer;
-  div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
   &:hover {
     background-color: rgba(74, 20, 140, 0.03);
   }
@@ -34,25 +27,23 @@ const TableRow = ({ row }) => {
   const store = useContext(StoreContext)
   const { activeNodeArray } = store
   const { projectId } = useParams()
-  const navigate = useNavigate()
 
   const project: Project = useLiveQuery(
     async () => await dexie.projects.where({ id: projectId }).first(),
   )
 
-  const onClickRow = useCallback(
-    () => navigate(`/${[...activeNodeArray, row.id].join('/')}`),
-    [activeNodeArray, navigate, row.id],
-  )
   const label =
     project?.use_labels === 1 && row.label
       ? row.label
       : row.name ?? '(unbenannt)'
 
   return (
-    <Row onClick={onClickRow}>
+    <StyledListItem
+      component={Link}
+      to={`/${[...activeNodeArray, row.id].join('/')}`}
+    >
       <div>{label}</div>
-    </Row>
+    </StyledListItem>
   )
 }
 
