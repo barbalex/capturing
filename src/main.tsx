4a03186852268d2e20e90352eb01f4a86e36594c
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-12 15:15:42 +0200

    remove simplebar

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import 'simplebar/dist/simplebar.min.css'
 
 import './globals.css'
 import App from './App'
