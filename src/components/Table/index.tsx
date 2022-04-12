f5eba6fa6285d445cf7e6c8761fc7828e0818b6b
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-12 13:14:18 +0200

    work on table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,13 +1,14 @@
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import { dexie, Table } from '../../dexieClient'
+import { dexie, Table, IProjectUser } from '../../dexieClient'
 import { useLiveQuery } from 'dexie-react-hooks'
 
 import ErrorBoundary from '../shared/ErrorBoundary'
 import Spinner from '../shared/Spinner'
 import FormTitle from './FormTitle'
 import Form from './Form'
+import { supabase } from '../../supabaseClient'
 
 const Container = styled.div`
   height: 100%;
@@ -16,14 +17,31 @@ const Container = styled.div`
   background-color: ${(props) => (props.showfilter ? '#fff3e0' : 'unset')};
 `
 
+type DataProps = {
+  row: Table
+  projectUser: IProjectUser
+}
+
 const TableComponent = ({ filter: showFilter }) => {
-  const { tableId } = useParams()
+  const session = supabase.auth.session()
+  const { tableId, projectId } = useParams()
   const filter = 'TODO: was in store'
 
-  const row: Table = useLiveQuery(
-    async () => await dexie.ttables.get(tableId),
-    [tableId],
-  )
+  const data: DataProps = useLiveQuery(async () => {
+    const [row, projectUser] = await Promise.all(
+      dexie.ttables.get(tableId),
+      dexie.project_users
+        .where({
+          project_id: projectId,
+          user_email: session?.user?.email,
+        })
+        .first(),
+    )
+    return { row, projectUser }
+  }, [tableId])
+  const row = data?.row
+  const userRole = data?.projectUser?.role
+  const userMayEdit = ['project_manager', 'project_editor'].includes(userRole)
 
   // console.log('Project rendering row:', { row, projectId })
 
@@ -33,7 +51,11 @@ const TableComponent = ({ filter: showFilter }) => {
   return (
     <ErrorBoundary>
       <Container showfilter={showFilter}>
-        <FormTitle row={row} showFilter={showFilter} />
+        <FormTitle
+          row={row}
+          showFilter={showFilter}
+          userMayEdit={userMayEdit}
+        />
         <Form showFilter={showFilter} id={tableId} row={row} />
       </Container>
     </ErrorBoundary>
