d093087fd0014d9229f36d98b9d8eb980996b654
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-13 17:20:03 +0200

    show fields nav only to manager

--- src/components/Table/FormTitle/NavButtons.tsx
+++ src/components/Table/FormTitle/NavButtons.tsx
@@ -3,13 +3,31 @@ import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
 import { FaArrowUp, FaArrowRight } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { Session } from '@supabase/supabase-js'
 
 import StoreContext from '../../../storeContext'
+import { dexie, IProjectUser } from '../../../dexieClient'
+import { supabase } from '../../../supabaseClient'
 
 const ProjectNavButtons = () => {
+  const { projectId } = useParams()
+  const session: Session = supabase.auth.session()
   const store = useContext(StoreContext)
   const { activeNodeArray, removeOpenNode } = store
+
+  const projectUser: IProjectUser = useLiveQuery(
+    async () =>
+      await dexie.project_users
+        .where({
+          project_id: projectId,
+          user_email: session?.user?.email,
+        })
+        .first(),
+  )
+  const userRole = projectUser?.role
+  const userMayManage = userRole === 'project_manager'
 
   const onClickUp = useCallback(() => {
     removeOpenNode(activeNodeArray)
@@ -26,9 +44,11 @@ const ProjectNavButtons = () => {
       >
         <FaArrowUp />
       </IconButton>
-      <Button endIcon={<FaArrowRight />} component={Link} to="fields">
-        Felder
-      </Button>
+      {!!userMayManage && (
+        <Button endIcon={<FaArrowRight />} component={Link} to="fields">
+          Felder
+        </Button>
+      )}
       <Button endIcon={<FaArrowRight />} component={Link} to="rows">
         Datensätze
       </Button>
