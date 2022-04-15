73d841681de418d20ca26133280ddbb5fceb70d7
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-15 22:18:01 +0200

    clean

--- src/utils/fetchFromServer/processTable.ts
+++ src/utils/fetchFromServer/processTable.ts
@@ -1,5 +1,5 @@
 import { supabase } from '../../supabaseClient'
-import { dexie, IProject } from '../../dexieClient'
+import { dexie } from '../../dexieClient'
 
 const fallbackRevAt = '1970-01-01T00:01:0.0Z'
 
