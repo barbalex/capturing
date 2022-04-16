1b789699fd068b17dd61b9d9174ecaf136a8d8ac
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-16 14:44:42 +0200

    optimize

--- src/components/Field/FormTitle/AddButton.tsx
+++ src/components/Field/FormTitle/AddButton.tsx
@@ -1,22 +1,19 @@
-import React, { useContext, useCallback } from 'react'
-import { observer } from 'mobx-react-lite'
+import React, { useCallback } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import IconButton from '@mui/material/IconButton'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, resolvePath } from 'react-router-dom'
 
 import ErrorBoundary from '../../shared/ErrorBoundary'
 import insertField from '../../../utils/insertField'
-import StoreContext from '../../../storeContext'
 
 const FieldAddButton = ({ userMayEdit }) => {
-  const { activeNodeArray } = useContext(StoreContext)
   const { tableId } = useParams()
   const navigate = useNavigate()
 
   const onClick = useCallback(async () => {
     const newId = await insertField({ tableId })
-    navigate(`/${[...activeNodeArray.slice(0, -1), newId].join('/')}`)
-  }, [activeNodeArray, navigate, tableId])
+    navigate(resolvePath`../${newId}`, window.location.pathname)
+  }, [navigate, tableId])
 
   return (
     <ErrorBoundary>
@@ -33,4 +30,4 @@ const FieldAddButton = ({ userMayEdit }) => {
   )
 }
 
-export default observer(FieldAddButton)
+export default FieldAddButton
