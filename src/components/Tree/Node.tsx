950348c8051b97aef943c532047d6579941fed34
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-24 13:11:46 +0200

    work on tree

--- src/components/Tree/Node.tsx
+++ src/components/Tree/Node.tsx
@@ -2,42 +2,52 @@ import { useContext } from 'react'
 import {
   MdChevronRight as ChevronRightIcon,
   MdExpandMore as ExpandMoreIcon,
-  MdMoreHoriz as MoreHorizIcon, // loading
 } from 'react-icons/md'
+import IconButton from '@mui/material/IconButton'
 import styled from 'styled-components'
+import isUuid from 'is-uuid'
+import last from 'lodash/last'
 
 import storeContext from '../../storeContext'
 
 const Container = styled.div``
 const Indent = styled.div`
-  font-weight: ${(props) => (props.isActive ? 'bold' : 'normal')};
-  ${(props) => props.isSelected && 'color: red;'}
+  display: flex;
+  align-items: center;
+  font-weight: ${(props) => (props.isInActiveNodeArray ? 'bold' : 'normal')};
+  ${(props) => props.isActive && 'color: red;'}
 `
-const Label = styled.span``
+const Label = styled.span`
+  font-size: 0.9em;
+`
 
 const Node = ({ innerRef, data, styles, handlers, state, tree }) => {
   const store = useContext(storeContext)
   const { activeNodeArray } = store
-  const isActive = activeNodeArray.includes(data.id)
-  console.log('Node', {
-    data,
-    isActive,
-    activeNodeArray: activeNodeArray.slice(),
-    isSelected: state.isSelected,
-  })
+  const isInActiveNodeArray = activeNodeArray.includes(data.id)
+  const isActive = data.id === last(activeNodeArray.filter((e) => isUuid.v1(e)))
+  // console.log('Node', {
+  //   data,
+  //   isInActiveNodeArray,
+  //   activeNodeArray: activeNodeArray.slice(),
+  //   isSelected: state.isSelected,
+  // })
 
   return (
     <Container ref={innerRef} style={styles.row} onClick={handlers.select}>
       <Indent
         style={styles.indent}
-        isActive={isActive}
+        isInActiveNodeArray={isInActiveNodeArray}
         isSelected={state.isSelected}
+        isActive={isActive}
       >
-        {state.isOpen ? (
-          <ExpandMoreIcon onClick={handlers.toggle} />
-        ) : (
-          <ChevronRightIcon onClick={handlers.toggle} />
-        )}
+        <IconButton aria-label="toggle" size="small">
+          {state.isOpen ? (
+            <ExpandMoreIcon onClick={handlers.toggle} />
+          ) : (
+            <ChevronRightIcon onClick={handlers.toggle} />
+          )}
+        </IconButton>
         <Label>{data.label}</Label>
       </Indent>
     </Container>
