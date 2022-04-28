7a2f197dbafff1c3a72122d7abd8d88ee3d77895
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-28 12:26:11 +0200

    work on project users

--- src/components/Project/ProjectUsers/index.tsx
+++ src/components/Project/ProjectUsers/index.tsx
@@ -1,4 +1,11 @@
+import { useCallback, useState } from 'react'
 import styled from 'styled-components'
+import { motion, useAnimation } from 'framer-motion'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
+import IconButton from '@mui/material/IconButton'
+
+import ErrorBoundary from '../../shared/ErrorBoundary'
+import constants from '../../../utils/constants'
 
 const TitleRow = styled.div`
   background-color: rgba(248, 243, 254, 1);
@@ -26,6 +33,27 @@ const Title = styled.div`
 `
 
 const ProjectUsers = () => {
+  const [open, setOpen] = useState(false)
+  let anim = useAnimation()
+  const onClickToggle = useCallback(
+    async (e) => {
+      e.stopPropagation()
+      if (open) {
+        const was = open
+        await anim.start({ opacity: 0 })
+        await anim.start({ height: 0 })
+        setOpen(!was)
+      } else {
+        setOpen(!open)
+        setTimeout(async () => {
+          await anim.start({ height: 'auto' })
+          await anim.start({ opacity: 1 })
+        })
+      }
+    },
+    [anim, open],
+  )
+
   return <div>Project-Users</div>
 }
 
