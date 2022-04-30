948ddd4f7543dc2d6a1ec065a02f626d4334e9a2
Alexander Gabriel <alex.barbalex@gmail.com>
2022-04-30 23:51:14 +0200

    work on map

--- src/components/Map/LocationMarker.tsx
+++ src/components/Map/LocationMarker.tsx
@@ -5,9 +5,6 @@ const LocationMarker = () => {
   const [position, setPosition] = useState(null)
 
   const map = useMapEvents({
-    click() {
-      map.locate()
-    },
     locationfound(e) {
       setPosition(e.latlng)
       map.flyTo(e.latlng, map.getZoom())
