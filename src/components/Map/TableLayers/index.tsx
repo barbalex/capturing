a6e36db5dba00a8205e7a0a73b626c3e6fa71ea7
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-01 18:39:43 +0200

    work on table layers

--- src/components/Map/TableLayers/index.tsx
+++ src/components/Map/TableLayers/index.tsx
@@ -1,4 +1,24 @@
+import { useLiveQuery } from 'dexie-react-hooks'
+import { useParams } from 'react-router-dom'
+
+import { dexie, Table } from '../../../dexieClient'
+import sortByLabelName from '../../../utils/sortByLabelName'
+
 const TableLayers = () => {
+  const { projectId } = useParams()
+
+  const tables: Table[] = useLiveQuery(
+    async () =>
+      await dexie.ttables
+        .where({ deleted: 0, project_id: projectId })
+        .toArray(),
+    [projectId],
+  )
+  const tablesSorted = sortByLabelName({
+    objects: tables,
+    useLabels,
+  })
+
   return null
 }
 
