c04414f0ea6ae69ecafee93ce2a4014056c639ab
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-01 18:25:48 +0200

    up db version

--- src/dexieClient.ts
+++ src/dexieClient.ts
@@ -1126,7 +1126,7 @@ export class MySubClassedDexie extends Dexie {
 
   constructor() {
     super('capturing')
-    this.version(22).stores({
+    this.version(23).stores({
       accounts: 'id, server_rev_at, deleted',
       field_types: 'id, &value, sort, server_rev_at, deleted',
       fields:
