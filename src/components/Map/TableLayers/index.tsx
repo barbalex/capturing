961880fdaeae754f6402dbbeb40fb47f61759a1b
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-02 12:12:02 +0200

    work on table layers

--- src/components/Map/TableLayers/index.tsx
+++ src/components/Map/TableLayers/index.tsx
@@ -1,16 +1,19 @@
+import { useCallback } from 'react'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { useParams } from 'react-router-dom'
 
 import { dexie, Table, Row } from '../../../dexieClient'
 import TableLayer from './TableLayer'
 
+const getStyle = (feature) => ({ color: feature.properties.color })
+
 const TableLayers = () => {
-  // bBeware: projectId can be undefined
-  const { projectId } = useParams()
+  const { projectId, rowId } = useParams()
 
   const layers = useLiveQuery(async () => {
     const where = projectId
-      ? { deleted: 0, project_id: projectId }
+      ? // Beware: projectId can be undefined and dexie does not like that
+        { deleted: 0, project_id: projectId }
       : { deleted: 0 }
     const tables: Table[] = await dexie.ttables.where(where).toArray()
 
@@ -22,11 +25,17 @@ const TableLayers = () => {
             row.deleted === 0 && row.table_id === table.id && !!row.geometry,
         )
         .toArray()
+      // convert geometry collection into feature collection to add properties (color)
+      const features = rows.map((e) => ({
+        geometry: e.geometry,
+        type: 'Feature',
+        properties: e.id === rowId ? { color: 'red' } : { color: 'green' },
+      }))
+      const data = { type: 'FeatureCollection', features }
+      console.log('TableLayers', { table, data })
 
       if (rows.length)
-        _layers.push(
-          <TableLayer key={table.id} data={rows.map((e) => e.geometry)} />,
-        )
+        _layers.push(<TableLayer key={table.id} data={data} style={getStyle} />)
     }
 
     return _layers
