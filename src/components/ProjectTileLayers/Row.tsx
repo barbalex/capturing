07377315151d401878fc31af6ade61fdd71b3320
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-04 11:57:01 +0200

    work on tyle layers list

--- src/components/ProjectTileLayers/Row.tsx
+++ src/components/ProjectTileLayers/Row.tsx
@@ -1,18 +1,32 @@
+import { useCallback } from 'react'
 import styled from 'styled-components'
-import ListItem from '@mui/material/ListItem'
-import { Link } from 'react-router-dom'
+import Checkbox from '@mui/material/Checkbox'
+import { useNavigate } from 'react-router-dom'
+import { Session } from '@supabase/supabase-js'
 
 import constants from '../../utils/constants'
-import { ProjectTileLayer } from '../../dexieClient'
+import { dexie, ProjectTileLayer } from '../../dexieClient'
+import { supabase } from '../../supabaseClient'
 
-const StyledListItem = styled(ListItem)`
-  min-height: ${constants.singleRowHeight};
+const Container = styled.div`
+  height: ${constants.singleRowHeight}px;
   border-top: thin solid rgba(74, 20, 140, 0.1);
   border-bottom: ${(props) => (props['data-last'] ? '1px' : 'thin')} solid
     rgba(74, 20, 140, 0.1);
   border-collapse: collapse;
   margin: -1px 0;
-  padding: 10px;
+  padding: 0 15px 0 0;
+  display: flex;
+  align-items: center;
+`
+const RowLink = styled.div`
+  width: calc(100% - 50px);
+  height: ${constants.singleRowHeight}px;
+  padding: 0 0 0 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
   &:hover {
     background-color: rgba(74, 20, 140, 0.03);
   }
@@ -22,10 +36,35 @@ type Props = {
 }
 
 const ProjectTilelayerComponent = ({ row }: Props) => {
+  const navigate = useNavigate()
+  const session: Session = supabase.auth.session()
+
+  const onClickActive = useCallback(
+    async (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      const was = { ...row }
+      const active = row.active === 1 ? 0 : 1
+      await dexie.project_tile_layers.update(row.id, {
+        active,
+      })
+
+      row.updateOnServer({
+        was,
+        is: { ...was, active },
+        session,
+      })
+    },
+    [row, session],
+  )
+
+  const onClickRow = useCallback(() => navigate(row.id), [navigate, row.id])
+
   return (
-    <StyledListItem component={Link} to={row.id}>
-      {row.label}
-    </StyledListItem>
+    <Container>
+      <RowLink onClick={onClickRow}>{row.label}</RowLink>
+      <Checkbox checked={row.active === 1} onClick={onClickActive} />
+    </Container>
   )
 }
 
