b41e3a265f991505ef3775a9fb2d737006037c56
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-04 12:21:58 +0200

    work on maps

--- src/components/Map/TileLayers/index.tsx
+++ src/components/Map/TileLayers/index.tsx
@@ -1,7 +1,7 @@
 import { useLiveQuery } from 'dexie-react-hooks'
 import { useParams } from 'react-router-dom'
 
-import { dexie, TileLayer as TileLayerType } from '../../../dexieClient'
+import { dexie, ProjectTileLayer as TileLayerType } from '../../../dexieClient'
 import TileLayer from './TileLayer'
 import OsmColor from '../layers/OsmColor'
 
@@ -12,16 +12,31 @@ const TileLayers = () => {
       { deleted: 0, active: 1, project_id: projectId }
     : { deleted: 0, active: 1 }
 
-  const tileLayers: TileLayerType[] = useLiveQuery(
-    async () => await dexie.project_tile_layers.where(where).sortBy('sort'),
-    [projectId],
-  )
+  const tileLayers: TileLayerType[] =
+    useLiveQuery(
+      async () => await dexie.project_tile_layers.where(where).sortBy('sort'),
+      [projectId],
+    ) ?? []
+  /**
+   * Ensure needed data exists:
+   * - url_template has template
+   * - wms has base-url and layers
+   */
+  const validTileLayers = tileLayers.filter((l) => {
+    if (!l.type) return false
+    if (l.type === 'url_template') {
+      if (!l.url_template) return false
+    } else {
+      if (!l.wms_base_url) return false
+      if (!l.wms_layers) return false
+    }
+    return true
+  })
 
-  if (!tileLayers) return []
   // is no tile layer was yet defined, use osm
-  if (!tileLayers.length) return [<OsmColor key="osm" />]
+  if (!validTileLayers.length) return [<OsmColor key="osm" />]
 
-  return tileLayers.map((layer) => (
+  return validTileLayers.map((layer) => (
     // use greyscale in key to redraw when user toggles it
     <TileLayer key={JSON.stringify(layer)} layer={layer} />
   ))
