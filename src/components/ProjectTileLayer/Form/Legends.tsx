50aeb8eb7957c2702da92f90d2f41801e1d432b6
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-05 21:40:21 +0200

    work on legends

--- src/components/ProjectTileLayer/Form/Legends.tsx
+++ src/components/ProjectTileLayer/Form/Legends.tsx
@@ -1,9 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import axios from 'redaxios'
+import styled from 'styled-components'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
+import IconButton from '@mui/material/IconButton'
+import { motion, useAnimation } from 'framer-motion'
 
 import ErrorBoundary from '../../shared/ErrorBoundary'
 import { Row } from '../../../dexieClient'
 import Spinner from '../../shared/Spinner'
+import constants from '../../../utils/constants'
+
+const Container = styled.div`
+  margin: 25px -10px 10px -10px;
+`
+const TitleRow = styled.div`
+  background-color: rgba(248, 243, 254, 1);
+  flex-shrink: 0;
+  display: flex;
+  height: ${constants.titleRowHeight}px;
+  justify-content: space-between;
+  padding: 0 10px;
+  cursor: pointer;
+  user-select: none;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  &:first-of-type {
+    margin-top: -10px;
+  }
+`
+const Title = styled.div`
+  font-weight: bold;
+  margin-top: auto;
+  margin-bottom: auto;
+`
 
 type Props = {
   row: Row
@@ -52,23 +82,67 @@ const ProjectTileLayerFormLegends = ({ row }: Props) => {
       run()
     }
   }, [row])
+  const [open, setOpen] = useState(false)
+  const anim = useAnimation()
+  const onClickToggle = useCallback(
+    async (e) => {
+      e.stopPropagation()
+      if (open) {
+        const was = open
+        await anim.start({ opacity: 0 })
+        await anim.start({ height: 0 })
+        setOpen(!was)
+      } else {
+        setOpen(!open)
+        setTimeout(async () => {
+          await anim.start({ height: 'auto' })
+          await anim.start({ opacity: 1 })
+        })
+      }
+    },
+    [anim, open],
+  )
 
   if (!row) return <Spinner />
 
   return (
     <ErrorBoundary>
-      <div>Legenden</div>
-      {(legends ?? []).map((l) => {
-        const title = l[0]
-        const blob = l[1]
-
-        return (
-          <div key={title}>
-            <div>{title}</div>
-            {!!blob && <img src={blob} />}
+      <Container>
+        <TitleRow
+          onClick={onClickToggle}
+          title={open ? 'schliessen' : 'öffnen'}
+        >
+          <Title>{`Styling von Geometrien`}</Title>
+          <div>
+            <IconButton
+              aria-label={open ? 'schliessen' : 'öffnen'}
+              title={open ? 'schliessen' : 'öffnen'}
+              onClick={onClickToggle}
+              size="large"
+            >
+              {open ? <FaChevronUp /> : <FaChevronDown />}
+            </IconButton>
           </div>
-        )
-      })}
+        </TitleRow>
+        <motion.div animate={anim} transition={{ type: 'just', duration: 0.2 }}>
+          {open && !!row && (
+            <>
+              <div>Legenden</div>
+              {(legends ?? []).map((l) => {
+                const title = l[0]
+                const blob = l[1]
+
+                return (
+                  <div key={title}>
+                    <div>{title}</div>
+                    {!!blob && <img src={blob} />}
+                  </div>
+                )
+              })}
+            </>
+          )}
+        </motion.div>
+      </Container>
     </ErrorBoundary>
   )
 }
