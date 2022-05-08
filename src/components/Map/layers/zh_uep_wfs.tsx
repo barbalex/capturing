99638035fbfbb5ea9565be5eb58ba7ab9fc4266b
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-08 16:04:54 +0200

    fight wfs filtering dragons

--- src/components/Map/layers/zh_uep_wfs.tsx
+++ src/components/Map/layers/zh_uep_wfs.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { GeoJSON } from 'react-leaflet'
+import { GeoJSON, useMap, useMapEvent } from 'react-leaflet'
 import axios from 'redaxios'
 import XMLViewer from 'react-xml-viewer'
 import Dialog from '@mui/material/Dialog'
@@ -22,25 +22,56 @@ const StyledDialogContent = styled(DialogContent)`
 `
 
 const ZhUepWfs = () => {
+  const map = useMapEvent('zoomend', () => {
+    console.log('ZhUepWfs zoomend, zoom:', map.getZoom())
+    setZoom(map.getZoom())
+  })
   const [error, setError] = useState()
   const [data, setData] = useState()
+  const [zoom, setZoom] = useState(map.getZoom())
+  const bounds = map.getBounds()
+  const bbox = `${bounds._northEast.lng},${bounds._northEast.lat},${bounds._southWest.lng},${bounds._southWest.lat}`
+
+  const filterOrig = `<Filter><BBOX><PropertyName>ms:ogd-0075_afv_gv_radwege_l</PropertyName><gml:Box srsName='EPSG:4326'><coordinates>${bounds._southWest.lng},${bounds._southWest.lat} ${bounds._northEast.lng},${bounds._northEast.lat}</coordinates></gml:Box></BBOX></Filter>`
+
+  const filter = `<ogc:Filter><ogc:BBOX><gml:Box srsName="EPSG:4326"><gml:coordinates decimal="." cs="," ts=" ">${bounds._southWest.lng},${bounds._southWest.lat} ${bounds._northEast.lng},${bounds._northEast.lat}</gml:coordinates></gml:Box></ogc:BBOX></ogc:Filter>`
+
+  const filterCQL = `BBOX(the_geom, ${bounds._northEast.lng}, ${bounds._northEast.lat}, ${bounds._southWest.lng}, ${bounds._southWest.lat})`
+
+  // const filterDifferent = `<Filter><Within><PropertyName>ms:ogd-0075_afv_gv_radwege_l<PropertyName><gml:Envelope><gml:lowerCorner>${bounds._southWest.lng} -${bounds._southWest.lat}</gml:lowerCorner><gml:upperCorner>${bounds._northEast.lng} -${bounds._northEast.lat}</gml:upperCorner></gml:Envelope></Within></Filter>`
+
   useEffect(() => {
     const run = async () => {
+      if (zoom < 9) return
       let res
       try {
         res = await axios({
           method: 'get',
-          url: 'http://maps.zh.ch/wfs/OGDZHWFS',
+          // url: 'https://maps.zh.ch/wfs/OGDZHWFS',
+          url: 'https://ows.geo.tg.ch/geofy_access_proxy/vernetzungskorridore',
           params: {
             service: 'WFS',
             version: '2.0.0',
+            // version: '1.1.0',
             // version: '2.0', // use to provoke error
             request: 'GetFeature',
-            typename: 'ms:ogd-0075_afv_gv_radwege_l',
-            srsname: 'EPSG:4326',
-            outputFormat: 'application/json; subtype=geojson',
+            // typenames:
+            //   'ms:ogd-0075_afv_gv_radwege_l,ms:ogd-0410_arv_basis_avzh_rohrleitungen_l',
+            // typename: 'ms:ogd-0410_arv_basis_avzh_rohrleitungen_l',
+            // typeName: 'ms:ogd-0075_afv_gv_radwege_l',
+            typeName: 'ms:vernetz',
+            // typeName: 'poi_aussichtspunkt_view',
+            // typename: 'ms:ogd-0346_giszhpub_dtmzh_hoehen_2017_l', // does not work?
+            // typename: 'ms:ogd-0119_giszhpub_feuchtgebietinv_79_90_beob_p', // does not work?
+            srsName: 'EPSG:4326',
+            // outputFormat: 'application/json; subtype=geojson',
+            // outputFormat: 'application/json',
+            outputFormat: 'GEOJSON',
+            // cql_filter: filterCQL,
+            // filter,
+            // bbox,
           },
-          dataType: 'jsonp',
+          // dataType: 'jsonp',
         })
       } catch (error) {
         // error can also be caused by timeout
@@ -52,26 +83,37 @@ const ZhUepWfs = () => {
       setData(res.data)
     }
     run()
-  }, [])
+  }, [zoom])
 
-  console.log('ZhUepWfs', { data, error })
+  console.log('ZhUepWfs', {
+    data,
+    // typeofData: typeof data,
+    // dataParsed: typeof data === 'string' ? JSON.parse(data) : data,
+    error,
+    zoom,
+    // bounds,
+    // bbox,
+    // filter,
+  })
 
   // TODO: add name of layer to title
   // TODO: get styling from layer_styles
   return (
     <>
-      <GeoJSON
-        key={data ? 1 : 0}
-        data={data}
-        // style={{
-        //   color: 'red',
-        //   stroke: 10,
-        //   weight: 1,
-        //   opacity: 1,
-        //   fill: 1,
-        //   fillColor: 'red',
-        // }}
-      />
+      {zoom > 8 && (
+        <GeoJSON
+          key={data ? 1 : 0}
+          data={data}
+          // style={{
+          //   color: 'red',
+          //   stroke: 10,
+          //   weight: 1,
+          //   opacity: 1,
+          //   fill: 1,
+          //   fillColor: 'red',
+          // }}
+        />
+      )}
       <Dialog
         onClose={() => setError(null)}
         open={!!error}
