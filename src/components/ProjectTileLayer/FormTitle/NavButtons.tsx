2c27871f8ac84d43b3b1d0e341510b24185f7287
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-16 20:33:56 +0200

    work on horizontal nav

--- src/components/ProjectTileLayer/FormTitle/NavButtons.tsx
+++ src/components/ProjectTileLayer/FormTitle/NavButtons.tsx
@@ -1,14 +1,38 @@
 import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
-import { FaArrowUp } from 'react-icons/fa'
+import { FaArrowUp, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import { Link, resolvePath } from 'react-router-dom'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { useParams } from 'react-router-dom'
 
 import StoreContext from '../../../storeContext'
+import { dexie, ProjectTileLayer } from '../../../dexieClient'
 
-const TableNavButtons = () => {
+const ProjectTileLayerNavButtons = () => {
+  const { projectId, projectTileLayerId } = useParams()
   const store = useContext(StoreContext)
   const { activeNodeArray, removeNode } = store
+
+  const projectTileLayerIds: string[] =
+    useLiveQuery(async () => {
+      const projectTileLayers: ProjectTileLayer[] =
+        await dexie.project_tile_layers
+          .where({ deleted: 0, project_id: projectId })
+          .sortBy('sort')
+      return projectTileLayers.map((p) => p.id)
+    }, [projectId]) ?? []
+
+  const activeIndex = (projectTileLayerIds ?? []).indexOf(projectTileLayerId)
+  const previousId =
+    activeIndex > 0 ? projectTileLayerIds[activeIndex - 1] : activeIndex
+  const nextId =
+    activeIndex === projectTileLayerIds.length - 1
+      ? projectTileLayerIds[activeIndex]
+      : projectTileLayerIds[activeIndex + 1]
+  const parentPath = resolvePath(`..`, window.location.pathname)?.pathname
+  const previousPath = `${parentPath}/${previousId}`
+  const nextPath = `${parentPath}/${nextId}`
 
   const onClickUp = useCallback(() => {
     removeNode(activeNodeArray)
@@ -19,14 +43,32 @@ const TableNavButtons = () => {
       <IconButton
         title="Zur Liste"
         component={Link}
-        to={resolvePath(`..`, window.location.pathname)}
+        to={parentPath}
         onClick={onClickUp}
         size="large"
       >
         <FaArrowUp />
       </IconButton>
+      <IconButton
+        title="Zum vorigen"
+        component={Link}
+        to={previousPath}
+        size="large"
+        disabled={activeIndex === 0}
+      >
+        <FaArrowLeft />
+      </IconButton>
+      <IconButton
+        title="Zum nächsten"
+        component={Link}
+        to={nextPath}
+        size="large"
+        disabled={activeIndex === projectTileLayerIds.length - 1}
+      >
+        <FaArrowRight />
+      </IconButton>
     </>
   )
 }
 
-export default observer(TableNavButtons)
+export default observer(ProjectTileLayerNavButtons)
