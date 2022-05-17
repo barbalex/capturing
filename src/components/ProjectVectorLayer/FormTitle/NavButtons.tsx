5569751361ad1079bb44ab6e785eacf47f6c4823
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-17 11:59:53 +0200

    add horizontal nav

--- src/components/ProjectVectorLayer/FormTitle/NavButtons.tsx
+++ src/components/ProjectVectorLayer/FormTitle/NavButtons.tsx
@@ -1,14 +1,38 @@
 import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
-import { FaArrowUp } from 'react-icons/fa'
+import { FaArrowUp, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import { Link, resolvePath } from 'react-router-dom'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { useParams } from 'react-router-dom'
 
 import StoreContext from '../../../storeContext'
+import { dexie, ProjectVectorLayer } from '../../../dexieClient'
 
 const ProjectVectorLayerNavButtons = () => {
+  const { projectId, projectVectorLayerId } = useParams()
   const store = useContext(StoreContext)
   const { activeNodeArray, removeNode } = store
+
+  const projectVectorLayerIds: string[] =
+    useLiveQuery(async () => {
+      const projectVectorLayers: ProjectVectorLayer[] =
+        await dexie.project_vector_layers
+          .where({ deleted: 0, project_id: projectId })
+          .sortBy('sort')
+      return projectVectorLayers.map((p) => p.id)
+    }, [projectId]) ?? []
+
+  const parentPath = resolvePath(`..`, window.location.pathname)?.pathname
+  const activeIndex = projectVectorLayerIds.indexOf(projectVectorLayerId)
+  const previousId =
+    activeIndex > 0 ? projectVectorLayerIds[activeIndex - 1] : activeIndex
+  const previousPath = `${parentPath}/${previousId}`
+  const nextId =
+    activeIndex === projectVectorLayerIds.length - 1
+      ? projectVectorLayerIds[activeIndex]
+      : projectVectorLayerIds[activeIndex + 1]
+  const nextPath = `${parentPath}/${nextId}`
 
   const onClickUp = useCallback(() => {
     removeNode(activeNodeArray)
@@ -19,11 +43,29 @@ const ProjectVectorLayerNavButtons = () => {
       <IconButton
         title="Zur Liste"
         component={Link}
-        to={resolvePath(`..`, window.location.pathname)}
+        to={parentPath}
         onClick={onClickUp}
         size="large"
       >
         <FaArrowUp />
+      </IconButton>
+      <IconButton
+        title="Zur vorigen"
+        component={Link}
+        to={previousPath}
+        size="large"
+        disabled={activeIndex === 0}
+      >
+        <FaArrowLeft />
+      </IconButton>
+      <IconButton
+        title="Zur nächsten"
+        component={Link}
+        to={nextPath}
+        size="large"
+        disabled={activeIndex === projectVectorLayerIds.length - 1}
+      >
+        <FaArrowRight />
       </IconButton>
     </>
   )
