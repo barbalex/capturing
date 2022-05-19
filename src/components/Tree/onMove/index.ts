3ebf7f40391755716f9c5303d757c3f65f89b9db
Alexander Gabriel <alex.barbalex@gmail.com>
2022-05-19 21:59:24 +0200

    sort

--- src/components/Tree/onMove/index.ts
+++ src/components/Tree/onMove/index.ts
@@ -2,21 +2,21 @@ import onMoveVectorLayers from './onMoveVectorLayers'
 import onMoveTileLayers from './onMoveTileLayers'
 import onMoveField from './onMoveField'
 
-const onMove = async ({ idsMoved, folderDroppedIn, endIndex, rebuild }) => {
+const onMove = async ({ idsMoved, folderDroppedIn, endIndex, rebuildTree }) => {
   console.log('onMove', { idsMoved, folderDroppedIn, endIndex })
   // do not know how multiple nodes can be mooved at once?
   const idMoved = idsMoved[0]
   if (folderDroppedIn.includes('vectorLayersFolder')) {
     await onMoveVectorLayers({ idMoved, folderDroppedIn, endIndex })
-    rebuild()
+    rebuildTree()
   }
   if (folderDroppedIn.includes('tileLayersFolder')) {
     await onMoveTileLayers({ idMoved, folderDroppedIn, endIndex })
-    rebuild()
+    rebuildTree()
   }
   if (folderDroppedIn.includes('fieldsFolder')) {
     await onMoveField({ idMoved, folderDroppedIn, endIndex })
-    rebuild()
+    rebuildTree()
   }
 }
 
