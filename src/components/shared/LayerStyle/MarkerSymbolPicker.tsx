fd4b979c0be5bc1a697576f61480d69c05ee42b7
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-02 17:42:01 +0200

    work on map markers

--- src/components/shared/LayerStyle/MarkerSymbolPicker.tsx
+++ src/components/shared/LayerStyle/MarkerSymbolPicker.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import * as icons from 'react-icons/md'
 import styled from 'styled-components'
 
 const SymbolContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  height: 500px;
+  overflow: auto;
 `
 
 import Label from '../Label'
 
 const MarkerSymbolPicker = () => {
-  console.log('MarkerSymbolPicker, images:', icons)
+  // console.log('MarkerSymbolPicker, images:', icons)
+  const wantedIconKeys = useMemo(
+    () =>
+      Object.keys(icons)
+        .filter((key) => !key.endsWith('Mp'))
+        .filter((key) => !key.endsWith('K'))
+        .filter((key) => !key.endsWith('KPlus')),
+    [],
+  )
+  console.log('MarkerSymbolPicker, wantedIconKeys:', wantedIconKeys)
 
   return (
     <>
       <Label label="Symbol wählen" />
       <SymbolContainer>
-        {Object.keys(icons).map((Icon) => React.createElement(icons[Icon], {}))}
+        {wantedIconKeys.map((key) => {
+          const Component = icons[key]
+
+          return (
+            <Component
+              key={key}
+              style={{ fontSize: 'x-large', padding: '3px', cursor: 'hand' }}
+            />
+          )
+        })}
       </SymbolContainer>
     </>
   )
