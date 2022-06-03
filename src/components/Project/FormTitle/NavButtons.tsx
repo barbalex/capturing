c6bd5d988716244855bbc9be026797259abbeb6f
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-03 14:40:46 +0200

    style

--- src/components/Project/FormTitle/NavButtons.tsx
+++ src/components/Project/FormTitle/NavButtons.tsx
@@ -1,14 +1,24 @@
 import React, { useContext, useCallback } from 'react'
 import { observer } from 'mobx-react-lite'
 import IconButton from '@mui/material/IconButton'
-import { FaArrowUp, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import {
+  FaArrowUp,
+  FaArrowLeft,
+  FaArrowRight,
+  FaArrowDown,
+} from 'react-icons/fa'
 import { Link, resolvePath, useParams } from 'react-router-dom'
 import { useLiveQuery } from 'dexie-react-hooks'
+import styled from 'styled-components'
 
 import StoreContext from '../../../storeContext'
 import { MenuChildrenButton } from '../../Table/FormTitle/NavButtons'
 import { dexie } from '../../../dexieClient'
 import sortProjectsByLabelName from '../../../utils/sortProjectsByLabelName'
+
+const StyledFaArrowDown = styled(FaArrowDown)`
+  font-size: 1.75rem !important;
+`
 
 const ProjectNavButtons = () => {
   const { projectId } = useParams()
@@ -72,21 +82,21 @@ const ProjectNavButtons = () => {
         <FaArrowRight />
       </IconButton>
       <MenuChildrenButton
-        endIcon={<FaArrowRight />}
+        endIcon={<StyledFaArrowDown />}
         component={Link}
         to="tables"
       >
         Tabellen
       </MenuChildrenButton>
       <MenuChildrenButton
-        endIcon={<FaArrowRight />}
+        endIcon={<StyledFaArrowDown />}
         component={Link}
         to="tile-layers"
       >
         Bild-Karten
       </MenuChildrenButton>
       <MenuChildrenButton
-        endIcon={<FaArrowRight />}
+        endIcon={<StyledFaArrowDown />}
         component={Link}
         to="vector-layers"
       >
