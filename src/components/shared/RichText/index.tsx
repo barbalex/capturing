55352608e6a6b1833bb65e85c16dd08bcb64ae35
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-05 12:33:02 +0200

    update lexical

--- src/components/shared/RichText/index.tsx
+++ src/components/shared/RichText/index.tsx
@@ -1,33 +1,33 @@
-import React, { useRef, useCallback, useEffect } from 'react'
+import React, { useRef, useCallback } from 'react'
 import FormHelperText from '@mui/material/FormHelperText'
 import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 
-import LexicalComposer from '@lexical/react/LexicalComposer'
-import RichTextPlugin from '@lexical/react/LexicalRichTextPlugin'
-import ContentEditable from '@lexical/react/LexicalContentEditable'
+import { LexicalComposer } from '@lexical/react/LexicalComposer'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
-import AutoFocusPlugin from '@lexical/react/LexicalAutoFocusPlugin'
-import { $getRoot, $getSelection } from 'lexical'
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
 
 import { HeadingNode, QuoteNode } from '@lexical/rich-text'
 import { TableCellNode, TableNode, TableRowNode } from '@lexical/table'
 import { ListItemNode, ListNode } from '@lexical/list'
 import { CodeHighlightNode, CodeNode } from '@lexical/code'
 import { AutoLinkNode, LinkNode } from '@lexical/link'
-import LinkPlugin from '@lexical/react/LexicalLinkPlugin'
-import ListPlugin from '@lexical/react/LexicalListPlugin'
-import TablePlugin from '@lexical/react/LexicalTablePlugin'
-import MarkdownShortcutPlugin from '@lexical/react/LexicalMarkdownShortcutPlugin'
-import OnChangePlugin from '@lexical/react/LexicalOnChangePlugin'
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin'
+import { ListPlugin } from '@lexical/react/LexicalListPlugin'
+import { TablePlugin } from '@lexical/react/LexicalTablePlugin'
+import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin'
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
 
 import ToolbarPlugin from './ToolbarPlugin'
 import AutoLinkPlugin from './AutoLinkPlugin'
 import CodeHighlightPlugin from './CodeHighlightPlugin'
 import theme from './theme'
 import './styles.css'
+import ErrorBoundary from '../ErrorBoundary'
 
 const StyledFormControl = styled(FormControl)`
   padding-bottom: 19px !important;
@@ -95,60 +95,65 @@ const RichText = ({
     onBlur(fakeEvent)
   }, [name, onBlur, state])
 
-  // console.log('RichText rendering', { state: state.current, value })
+  console.log('RichText rendering', { state: state.current, value })
 
   // once schrink is set, need to manually control ist
   // schrink if value exists or schrinkLabel was passed
   const schrink = schrinkLabel || !!value || value === 0
 
   return (
-    <StyledFormControl
-      fullWidth
-      disabled={disabled}
-      error={!!error}
-      aria-describedby={`${label}ErrorText`}
-      variant="standard"
-    >
-      <StyledInputLabel
-        htmlFor={label}
-        shrink={schrink}
-        data-weight={labelWeight}
+    <ErrorBoundary>
+      <StyledFormControl
+        fullWidth
+        disabled={disabled}
+        error={!!error}
+        aria-describedby={`${label}ErrorText`}
+        variant="standard"
       >
-        {label}
-      </StyledInputLabel>
-      <Container onBlur={onBlurContainer}>
-        <LexicalComposer initialConfig={editorConfig}>
-          <div className="editor-container">
-            <ToolbarPlugin />
-            <div className="editor-inner">
-              <RichTextPlugin
-                contentEditable={<ContentEditable className="editor-input" />}
-                initialEditorState={value ? JSON.stringify(value) : undefined}
-              />
-              <HistoryPlugin />
-              <AutoFocusPlugin />
-              <ListPlugin />
-              <TablePlugin />
-              <LinkPlugin />
-              <AutoLinkPlugin />
-              <CodeHighlightPlugin />
-              <MarkdownShortcutPlugin />
-              <OnChangePlugin
-                onChange={(newState) => (state.current = newState)}
-                ignoreSelectionChange={true}
-                ignoreInitialChange={true}
-              />
+        <StyledInputLabel
+          htmlFor={label}
+          shrink={schrink}
+          data-weight={labelWeight}
+        >
+          {label}
+        </StyledInputLabel>
+        <Container onBlur={onBlurContainer}>
+          <LexicalComposer initialConfig={editorConfig}>
+            <div className="editor-container">
+              <ToolbarPlugin />
+              <div className="editor-inner">
+                <RichTextPlugin
+                  contentEditable={<ContentEditable className="editor-input" />}
+                  initialEditorState={value ? JSON.stringify(value) : undefined}
+                  // initialEditorState={undefined}
+                />
+                <HistoryPlugin />
+                <AutoFocusPlugin />
+                <ListPlugin />
+                <TablePlugin />
+                <LinkPlugin />
+                <AutoLinkPlugin />
+                <CodeHighlightPlugin />
+                <MarkdownShortcutPlugin />
+                <OnChangePlugin
+                  onChange={(newState) => (state.current = newState)}
+                  ignoreSelectionChange={true}
+                  ignoreInitialChange={true}
+                />
+              </div>
             </div>
-          </div>
-        </LexicalComposer>
-      </Container>
-      {!!error && (
-        <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
-      )}
-      {!!helperText && (
-        <FormHelperText id={`${label}HelperText`}>{helperText}</FormHelperText>
-      )}
-    </StyledFormControl>
+          </LexicalComposer>
+        </Container>
+        {!!error && (
+          <FormHelperText id={`${label}ErrorText`}>{error}</FormHelperText>
+        )}
+        {!!helperText && (
+          <FormHelperText id={`${label}HelperText`}>
+            {helperText}
+          </FormHelperText>
+        )}
+      </StyledFormControl>
+    </ErrorBoundary>
   )
 }
 
