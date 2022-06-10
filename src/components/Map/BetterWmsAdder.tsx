f490de7128fe4bfc1e8faa6e1619f283f4c23b83
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-10 19:15:14 +0200

    comment

--- src/components/Map/BetterWmsAdder.tsx
+++ src/components/Map/BetterWmsAdder.tsx
@@ -1,3 +1,7 @@
+/**
+ * not in use
+ */
+
 import { useEffect } from 'react'
 import { useMap } from 'react-leaflet'
 import 'leaflet'
@@ -55,7 +59,7 @@ const BetterWmsAdder = () => {
               transparent: this.wmsParams.transparent,
               version: this.wmsParams.version,
               format: this.wmsParams.format,
-              bbox: this._map.getBounds().toBBoxString(), 
+              bbox: this._map.getBounds().toBBoxString(),
               height: size.y,
               width: size.x,
               layers: this.wmsParams.layers,
