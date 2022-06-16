ab0616a926613845984f1438ce583fb5d1718943
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-16 10:48:17 +0200

    remove not used

--- src/components/Map/TileLayers/TileLayer/WMTS.tsx
+++ src/components/Map/TileLayers/TileLayer/WMTS.tsx
@@ -1,15 +1,6 @@
 import { TileLayer } from 'react-leaflet'
-import styled from 'styled-components'
 
 import { TileLayer as TileLayerType } from '../../../../dexieClient'
-
-const StyledTileLayer = styled(TileLayer)`
-  ${(props) =>
-    props.greyscale == 1 &&
-    `.leaflet-tile-container {
-    filter: grayscale(100%) !important;
-  }`}
-`
 
 type Props = {
   layer: TileLayerType
@@ -17,12 +8,11 @@ type Props = {
 
 const WMTS = ({ layer }: Props) => {
   return (
-    <StyledTileLayer
+    <TileLayer
       url={layer.wmts_url_template}
       maxNativeZoom={19}
       minZoom={layer.min_zoom}
       maxZoom={layer.max_zoom}
-      greyscale={layer.greyscale}
       opacity={layer.opacity}
     />
   )
