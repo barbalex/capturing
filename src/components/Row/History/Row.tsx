5970ad502d8527fc32d22477d0d6a171228f8261
Alexander Gabriel <alex.barbalex@gmail.com>
2022-06-19 17:10:07 +0200

    work on history

--- src/components/Row/History/Row.tsx
+++ src/components/Row/History/Row.tsx
@@ -1,4 +1,18 @@
-const HistoryRow = ({ row, revRow }) => {
+import React, { useCallback, useContext, useMemo } from 'react'
+import { observer } from 'mobx-react-lite'
+import md5 from 'blueimp-md5'
+import { v1 as uuidv1 } from 'uuid'
+import isEqual from 'lodash/isEqual'
+
+import History from '../../shared/History'
+import storeContext from '../../../storeContext'
+import checkForOnlineError from '../../../utils/checkForOnlineError'
+
+
+const HistoryRow = ({ row, revRow, historyTakeoverCallback }) => {
+  const store = useContext(StoreContext)
+  const { user, addNotification,  } = store
+
   return <div>{JSON.stringify(revRow)}</div>
 }
 
