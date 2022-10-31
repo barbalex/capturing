ae002133105f829b3ab7f4aefe8aeb7d7a67f7bf
Alexander Gabriel <alex.barbalex@gmail.com>
2022-10-31 14:55:07 +0100

    format

--- supabase/migrations/20221031_01.sql
+++ supabase/migrations/20221031_01.sql
@@ -1,6 +1,6 @@
 -- add ability to relate tables to each other:
 ALTER TABLE fields
-  ADD COLUMN table_ref uuid default null REFERENCES tables (id) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD COLUMN table_ref uuid DEFAULT NULL REFERENCES tables (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 CREATE INDEX ON fields USING btree (table_ref);
 
