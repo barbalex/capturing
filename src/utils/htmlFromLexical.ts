0e1fff2df3aaff7e58d33217396219022f7b66fe
Alexander Gabriel <alex.barbalex@gmail.com>
2022-11-10 11:34:27 +0100

    add editorConfig

--- src/utils/htmlFromLexical.ts
+++ src/utils/htmlFromLexical.ts
@@ -1,11 +1,35 @@
 import { createEditor } from 'lexical'
 import { $generateHtmlFromNodes } from '@lexical/html'
 
-const editor = createEditor()
- 
+import { HeadingNode, QuoteNode } from '@lexical/rich-text'
+import { TableCellNode, TableNode, TableRowNode } from '@lexical/table'
+import { ListItemNode, ListNode } from '@lexical/list'
+import { CodeHighlightNode, CodeNode } from '@lexical/code'
+import { AutoLinkNode, LinkNode } from '@lexical/link'
+
+const editorConfig = {
+  onError: () => console.log(error),
+  nodes: [
+    HeadingNode,
+    ListNode,
+    ListItemNode,
+    QuoteNode,
+    CodeNode,
+    CodeHighlightNode,
+    TableNode,
+    TableCellNode,
+    TableRowNode,
+    AutoLinkNode,
+    LinkNode,
+  ],
+}
+
+const editor = createEditor(editorConfig)
+
 const htmlFromLexical = (value) =>
   new Promise((resolve) => {
     if (!value) return
+
     editor.parseEditorState(value, () => {
       const html = $generateHtmlFromNodes(editor, null)
       resolve(html)
