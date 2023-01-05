17877321f395063b8887ab6c51ae832dd192e47b
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-01-05 16:11:42 +0100

    modularize

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { onSnapshot } from 'mobx-state-tree'
 import isEqual from 'lodash/isEqual'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -8,16 +8,6 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { MobxStore } from './store'
 import materialTheme from './utils/materialTheme'
 import { Provider as MobxProvider } from './storeContext'
-import Home from './routes/Home'
-import Docs from './routes/Docs'
-import User from './routes/User'
-import ImageLayerTypes from './components/Docs/ImageLayerTypes'
-import OfflineMaps from './components/Docs/OfflineMaps'
-import DataSynchronisation from './components/Docs/DataSynchronisation'
-import DataVersioning from './components/Docs/DataVersioning'
-import DataHistory from './components/Docs/DataHistory'
-import ProjectsPage from './routes/Projects'
-import FourOhFour from './routes/404'
 import Layout from './components/Layout'
 import Notifications from './components/Notifications'
 import { dexie } from './dexieClient'
@@ -29,23 +19,11 @@ import QueuedUpdatesWriter from './components/QueuedUpdatesWriter'
 import { supabase } from './supabaseClient'
 import fetchFromServer from './utils/fetchFromServer'
 import ApiDetector from './components/ApiDetector'
-import ProjectsComponent from './components/Projects'
-import ProjectComponent from './components/Project'
-import TablesComponent from './components/Tables'
-import TableComponent from './components/Table'
-import TileLayersComponent from './components/TileLayers'
-import TileLayerComponent from './components/TileLayer'
-import VectorLayersComponent from './components/VectorLayers'
-import VectorLayerComponent from './components/VectorLayer'
-import FieldsComponent from './components/Fields'
-import FieldComponent from './components/Field'
-import RowsComponent from './components/Rows'
-import RowComponent from './components/Row'
+import RouterComponent from './components/Router'
 
 const queryClient = new QueryClient()
 
-// trying to persist indexedDB
-// https://dexie.org/docs/StorageManager#controlling-persistence
+// persisting indexedDB: https://dexie.org/docs/StorageManager#controlling-persistence
 // TODO: consider calling this only if user choose it in settings
 // or pop own window to explain as shown in above link
 // because it pops a request window
@@ -124,79 +102,7 @@ function App() {
               <QueuedUpdatesWriter />
               <ApiDetector />
               <Layout>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="docs/*" element={<Docs />}>
-                    <Route
-                      path="image-layer-types"
-                      element={<ImageLayerTypes />}
-                    />
-                    <Route path="offline-maps" element={<OfflineMaps />} />
-                    <Route
-                      path="data-synchronization"
-                      element={<DataSynchronisation />}
-                    />
-                    <Route
-                      path="data-versioning"
-                      element={<DataVersioning />}
-                    />
-                    <Route path="data-history" element={<DataHistory />} />
-                  </Route>
-                  <Route path="user" element={<User />} />
-                  <Route path="projects/*" element={<ProjectsPage />}>
-                    <Route index element={<ProjectsComponent />} />
-                    <Route path=":projectId" element={<ProjectComponent />} />
-                    <Route
-                      path=":projectId/tables/*"
-                      element={<TablesComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId"
-                      element={<TableComponent />}
-                    />
-                    <Route
-                      path=":projectId/tile-layers/*"
-                      element={<TileLayersComponent />}
-                    />
-                    <Route
-                      path=":projectId/tile-layers/:tileLayerId"
-                      element={<TileLayerComponent />}
-                    />
-                    <Route
-                      path=":projectId/vector-layers/*"
-                      element={<VectorLayersComponent />}
-                    />
-                    <Route
-                      path=":projectId/vector-layers/:vectorLayerId"
-                      element={<VectorLayerComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/fields/*"
-                      element={<FieldsComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/fields/:fieldId"
-                      element={<FieldComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/rows/*"
-                      element={<RowsComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/rows/:rowId/*"
-                      element={<RowComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/rows/:rowId/tables/:tableId2/*"
-                      element={<RowsComponent />}
-                    />
-                    <Route
-                      path=":projectId/tables/:tableId/rows/:rowId/tables/:tableId2/rows/:rowId2/*"
-                      element={<RowComponent />}
-                    />
-                  </Route>
-                  <Route path="*" element={<FourOhFour />} />
-                </Routes>
+                <RouterComponent />
               </Layout>
               <Notifications />
             </QueryClientProvider>
---
+++ src/components/Router.tsx
@@ -0,0 +1,89 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../routes/Home'
+import Docs from '../routes/Docs'
+import User from '../routes/User'
+import ImageLayerTypes from './Docs/ImageLayerTypes'
+import OfflineMaps from './Docs/OfflineMaps'
+import DataSynchronisation from './Docs/DataSynchronisation'
+import DataVersioning from './Docs/DataVersioning'
+import DataHistory from './Docs/DataHistory'
+import ProjectsPage from '../routes/Projects'
+import FourOhFour from '../routes/404'
+import ProjectsComponent from './Projects'
+import ProjectComponent from './Project'
+import TablesComponent from './Tables'
+import TableComponent from './Table'
+import TileLayersComponent from './TileLayers'
+import TileLayerComponent from './TileLayer'
+import VectorLayersComponent from './VectorLayers'
+import VectorLayerComponent from './VectorLayer'
+import FieldsComponent from './Fields'
+import FieldComponent from './Field'
+import RowsComponent from './Rows'
+import RowComponent from './Row'
+
+function RouterComponent() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="docs/*" element={<Docs />}>
+        <Route path="image-layer-types" element={<ImageLayerTypes />} />
+        <Route path="offline-maps" element={<OfflineMaps />} />
+        <Route path="data-synchronization" element={<DataSynchronisation />} />
+        <Route path="data-versioning" element={<DataVersioning />} />
+        <Route path="data-history" element={<DataHistory />} />
+      </Route>
+      <Route path="user" element={<User />} />
+      <Route path="projects/*" element={<ProjectsPage />}>
+        <Route index element={<ProjectsComponent />} />
+        <Route path=":projectId" element={<ProjectComponent />} />
+        <Route path=":projectId/tables/*" element={<TablesComponent />} />
+        <Route path=":projectId/tables/:tableId" element={<TableComponent />} />
+        <Route
+          path=":projectId/tile-layers/*"
+          element={<TileLayersComponent />}
+        />
+        <Route
+          path=":projectId/tile-layers/:tileLayerId"
+          element={<TileLayerComponent />}
+        />
+        <Route
+          path=":projectId/vector-layers/*"
+          element={<VectorLayersComponent />}
+        />
+        <Route
+          path=":projectId/vector-layers/:vectorLayerId"
+          element={<VectorLayerComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/fields/*"
+          element={<FieldsComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/fields/:fieldId"
+          element={<FieldComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/rows/*"
+          element={<RowsComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/rows/:rowId/*"
+          element={<RowComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/rows/:rowId/tables/:tableId2/*"
+          element={<RowsComponent />}
+        />
+        <Route
+          path=":projectId/tables/:tableId/rows/:rowId/tables/:tableId2/rows/:rowId2/*"
+          element={<RowComponent />}
+        />
+      </Route>
+      <Route path="*" element={<FourOhFour />} />
+    </Routes>
+  )
+}
+
+export default RouterComponent
