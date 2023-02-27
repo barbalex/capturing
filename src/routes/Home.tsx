b8f30b9bca1db678155046ee949106e031d3a972
Alexander Gabriel <alex.barbalex@gmail.com>
2023-02-27 18:12:05 +0100

    style

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -69,7 +69,7 @@ const Card = styled(MaterialCard)`
   outline: rgba(74, 20, 140, 0.3) 1px solid;
   font-weight: 700;
   font-size: 1.2em !important;
-  text-shadow: white 0 0 2px, rgba(74, 20, 140, 0.5) 0 0 5px;
+  text-shadow: white 0 0 2px, rgba(74, 20, 140, 0.4) 0 0 5px;
   ul {
     margin-bottom: 0;
   }
@@ -84,7 +84,7 @@ const PageTitle = styled(Typography)`
   font-size: 2.5em !important;
   padding-bottom: 15px;
   font-weight: 700 !important;
-  text-shadow: white 0 0 2px, rgba(74, 20, 140, 0.5) 0 0 5px;
+  text-shadow: white 0 0 2px, rgba(74, 20, 140, 0.4) 0 0 5px;
   color: #210148;
   @media (min-width: 700px) {
     padding-bottom: 20px;
@@ -119,7 +119,7 @@ const Img = styled.img`
   height: 100%;
   width: 100%;
   object-fit: cover;
-  filter: opacity(0.16);
+  filter: opacity(0.1);
 `
 
 const Home = () => {
