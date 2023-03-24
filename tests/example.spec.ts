b8bd6cc01c3f0dcfdf3a0f8eb71f1309ebc2868e
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-24 16:25:15 +0100

    add tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,3 +4,21 @@ test('has home title', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveTitle(/Erfassen: Home/)
 })
+
+test('Doku link', async ({ page }) => {
+  await page.goto(`/`)
+  await page.getByRole('link', { name: 'Doku' }).click()
+  await expect(page).toHaveURL('/docs')
+})
+
+test('Home link', async ({ page }) => {
+  await page.goto(`/docs`)
+  await page.getByRole('link', { name: 'Erfassen' }).click()
+  await expect(page).toHaveURL('/')
+})
+
+test('Daten link', async ({ page }) => {
+  await page.goto(`/`)
+  await page.getByRole('link', { name: 'Daten' }).click()
+  await expect(page).toHaveURL('/projects')
+})
